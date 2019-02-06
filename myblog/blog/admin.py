4525becd8a2bb2a9cd447b5fd52ebd6aeaa25d6e
lamalla <lm3gc@virginia.edu>
2019-02-05 22:02:09 -0500

    removed visitor model

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.register(Owner)
+admin.site.register(Post)
 admin.site.register(Comment)
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from datetime import datetime
 
+
+class Owner(models.Model):
+    name = models.CharField(max_length=200)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    author= models.ForeignKey(Owner, on_delete=models.CASCADE)
+    text= models.TextField(default=" ")
 
 class Comment(models.Model):
+    author= models.CharField(max_length=200)
     text = models.CharField(max_length=200)
-    date = models.DateTimeField('date posted')
+    date = models.DateTimeField()
+    post=  models.ForeignKey(Post, on_delete=models.CASCADE)
